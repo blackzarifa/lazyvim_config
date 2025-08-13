9027ddb0a16ff977b3e3198368130764e5ca23c2
blackzarifa <rafaelirades@gmail.com>
2025-08-12 21:59:42 -0300

    md and toml langs

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -28,6 +28,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- utils
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
