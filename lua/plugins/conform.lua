8c8b051df0e067803f3d1842122979297bb29db0
blackzarifa <rafaelirades@gmail.com>
2025-08-18 23:19:29 -0300

    feat golines in conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+  "stevearc/conform.nvim",
+  opts = function(_, opts)
+    opts.formatters_by_ft = opts.formatters_by_ft or {}
+
+    local go_formatters = opts.formatters_by_ft.go or {}
+    table.insert(go_formatters, "golines")
+    opts.formatters_by_ft.go = go_formatters
+
+    opts.formatters = opts.formatters or {}
+    opts.formatters.golines = {
+      command = "golines",
+      args = { "--max-len=100" },
+    }
+  end,
+}
