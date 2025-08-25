ff0d713e8fdfd850cab3a40caeec03273f770331
blackzarifa <rafaelirades@gmail.com>
2025-08-25 19:00:50 -0300

    feat changed lazyvim s bind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Remove LazyVim "search" for default Vim `s` bind
+vim.keymap.del("n", "s")
