e563338a3f6dd28f2ce7210a9913455f9b5e0614
blackzarifa <rafaelirades@gmail.com>
2025-09-02 11:42:09 -0300

    feat notification history

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,11 @@
 
 -- Remove LazyVim "search" for default Vim `s` bind
 vim.keymap.del("n", "s")
+
+-- Notification history with copy and actually working
+vim.keymap.set("n", "<leader>N", function()
+  Snacks.notifier.show_history({ win = { bo = { modifiable = true } } })
+  vim.defer_fn(function()
+    vim.wo.wrap = true
+  end, 10)
+end, { desc = "Readable Notification History" })
