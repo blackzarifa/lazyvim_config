0ce4400f3d802707e0b80cf2789e04325ac4970e
blackzarifa <rafaelirades@gmail.com>
2026-02-16 01:16:06 -0300

    refac removing some inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,28 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
+        vtsls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                parameterNames = { enabled = "none" },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+              },
+            },
+            javascript = {
+              inlayHints = {
+                parameterNames = { enabled = "none" },
+                parameterTypes = { enabled = false },
+                variableTypes = { enabled = false },
+                propertyDeclarationTypes = { enabled = false },
+                functionLikeReturnTypes = { enabled = false },
+              },
+            },
+          },
+        },
         gopls = {
           settings = {
             gopls = {
